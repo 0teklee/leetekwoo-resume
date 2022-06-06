8f0847de915da627ab2462e11e738faf4cc5209c
0teklee <leetekwoo@gmail.com>
2022-06-06 23:57:03 +0900

    [ADD] Spoqa 폰트 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,11 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+            <link
+              href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css'
+              rel='stylesheet'
+              type='text/css'
+            />
           </Head>
 
           <body>
